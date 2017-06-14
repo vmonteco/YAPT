bb2810e63b8d0a423e259b89db127ba464a65b2b
Vincent MONTECOT <vmonteco@e2r12p7.42.fr>
2017-06-14 04:47:01 +0200

    processes not being reaped after timeout problem should be solved.

--- yapt.py
+++ yapt.py
@@ -8,6 +8,8 @@ import ctypes
 import ctypes.util
 import imp
 import importlib
+import signal
+import time
 import subprocess
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -22,6 +24,38 @@ libc = ctypes.cdll.LoadLibrary(ctypes.util.find_library('c'))
 ft_printf = libftprintf.ft_printf
 printf = libc.printf
 
+# *************************************************************************** #
+#                                Defining texts.                              #
+# *************************************************************************** #
+
+class Timeout:
+
+    """
+    Context manager to handle operations taking to long time.
+    Raises a TimeoutError in such case (that would have to be handled by a
+    try/except block).
+    """
+    
+    def __init__(self, t=1, msg='This operation took too long'):
+        self.t = t
+        self.error_msg = msg
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_msg)
+
+    def __enter__(self):
+        if self.t != 0:
+            signal.signal(signal.SIGALRM, self.handle_timeout)
+            signal.setitimer(signal.ITIMER_REAL, self.t)
+            #signal.alarm(self.t)
+
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+        
+def timeout_handler(fd):
+    fd.write(str(-1))
+    sys.exit(1)
+        
 # *************************************************************************** #
 #                                Defining texts.                              #
 # *************************************************************************** #
@@ -41,7 +75,7 @@ msgs = {
     ),
     'test_err': (
         '[case: #%s][{case}%r{rst}] -> [%s/%s]'
-        '[{res1}%d{rst}/{res2}%d{rst}] ({res}%s{rst}).'
+        '[{res1}%s{rst}/{res2}%s{rst}] ({res}%s{rst}).'
     ),
     'exit_err': '{fail}%s cases exited non zero statuses.{rst}'
 }
@@ -68,13 +102,14 @@ class Tester:
         }
 
     def run(self, cases_generator=None, verbose=False, quiet=False,
-            debug=False):
+            timeout=0.15):
         """
         This is the main run() method. It will (or not) trigger other
         run submethods.
         """
         print(colorize(self.msgs['welcome']))
-        self.run_cmp_cases(cases_generator(), verbose, quiet, debug)
+        self.run_cmp_cases(cases_generator(), verbose=verbose, quiet=quiet,
+                           timeout=timeout)
         if self.counters['global_exit_err'] == 0:
             print('Running cases in current process...')
             cases = cases_generator()
@@ -95,49 +130,16 @@ class Tester:
                     'Leaks tests failed.{rst}\n'
                 )))
 
-    def run_in_subprocess(self, function, case):
-        """
-        This method run a test case by passing it to both f1 and f2,
-        and by running it in subprocesses.
-        It monitors the output on STDOUT, the function's return and the
-        child process exit status..
-        """
-        pipes = {}
-        pipes['output_r'], pipes['output_w'] = os.pipe()
-        pipes['return_r'], pipes['return_w'] = os.pipe()
-        pid = os.fork()
-        if (pid < 0):
-            raise Exception('unable to fork')
-        elif (pid == 0):
-            os.close(pipes['output_r'])
-            os.close(pipes['return_r'])
-            os.dup2(pipes['output_w'], sys.stdout.fileno())
-            return_fd = os.fdopen(pipes['return_w'], 'w')
-            ret = function(*case)
-            return_fd.write(str(ret))
-            return_fd.close()
-            os.close(pipes['output_w'])
-            sys.exit()
-        else:
-            res = {}
-            os.close(pipes['output_w'])
-            os.close(pipes['return_w'])
-            output_in = os.fdopen(pipes['output_r'], encoding='cp1252')
-            return_in = os.fdopen(pipes['return_r'])
-            res['output'] = output_in.read()
-            res['return'] = int(return_in.read())
-            res['status'] = os.waitpid(pid, 0)[1]
-            return res
-
-    def run_cmp_cases(self, cases, verbose=False, quiet=False, debug=False):
+    def run_cmp_cases(self, cases, verbose=False, quiet=False, debug=False,
+                      timeout=0.15):
         """
         This run submethod just run test sets by calling run_in_subprocess().
         """
         self.counters['global_tried'] = 0
         self.counters['global_success'] = 0
         for case in cases:
-            self.run_cmp_cases_subsets(case['name'], case, verbose, quiet,
-                                       debug)
+            self.run_cmp_cases_subsets(case['name'], case, verbose=verbose,
+                                       quiet=quiet, timeout=timeout)
         if self.counters['global_tried'] > 0:
             success = self.counters['global_tried'] == self.counters[
                 'global_success'
@@ -157,7 +159,7 @@ class Tester:
                   ))
 
     def run_cmp_cases_subsets(self, name, cases, verbose=False, quiet=False,
-                              debug=False):
+                              timeout=0.15):
         """
         This run submethod just run test subsets by calling
         run_in_subprocess().
@@ -167,7 +169,8 @@ class Tester:
         self.counters['local_exit_err'] = 0
         print(colorize(self.msgs['subset_head'], {}) % (cases['name'],))
         for case in cases['cases']:
-            self.run_cmp_case(name, case, verbose, quiet, debug)
+            self.run_cmp_case(name, case, verbose=verbose, quiet=quiet,
+                              timeout=timeout)
         if self.counters['local_tried'] > 0:
             success = (
                 self.counters['local_tried'] == self.counters['local_success']
@@ -184,23 +187,17 @@ class Tester:
                       % (
                           self.counters['local_exit_err']
                       ))
+
                 
-    def run_cmp_case(self, name, case, verbose=False, quiet=False, debug=False):
+    def run_cmp_case(self, name, case, verbose=False, quiet=False,
+                     timeout=0.15):
         """
         This method just runs an actual test by calling
         run_in_subprocess().
         """
-        if debug:
-            print("case : [%s] " % case, end='', file=sys.stderr)
-        f1 = self.run_in_subprocess(self.f1, case)
-        if debug:
-            print("f1 issued " % case, end='', file=sys.stderr)
-        f2 = self.run_in_subprocess(self.f2, case)
-        if debug:
-            print("f2 issued" % case, file=sys.stderr)
         res = {
-            'f1': f1,
-            'f2': f2
+            'f1': self.run_in_fork(self.f1, case, timeout=timeout),
+            'f2': self.run_in_fork(self.f2, case, timeout=timeout)
         }
         self.interpret_cmp_results(case, res, verbose, quiet)
         print('results : local (%s) : [%s/%s], global : [%s/%s]' % (
@@ -210,16 +207,71 @@ class Tester:
             self.counters['global_success'],
             self.counters['global_tried']
         ), end='\r')
+
+                
+    def run_in_fork(self, function, case, timeout=0.15):
+        """
+        This method runs a test case by passing it to both f1 and f2,
+        and by running it in subprocesses.
+        It monitors the output on STDOUT, the function's return and the
+        child process exit status..
+        """
+        pipes = {}
+        pipes['output_r'], pipes['output_w'] = os.pipe()
+        pipes['return_r'], pipes['return_w'] = os.pipe()
+        pid = os.fork()
+        if (pid < 0):
+            raise Exception('unable to fork')
+        elif (pid == 0):
+            try:
+                with Timeout(t=5) as t:
+                    os.close(pipes['output_r'])
+                    os.close(pipes['return_r'])
+                    os.dup2(pipes['output_w'], sys.stdout.fileno())
+                    return_fd = os.fdopen(pipes['return_w'], 'w')
+                    #signal.signal(signal.SIGTERM, lambda s, f: handle_timeout(return_fd))
+                    ret = function(*case)
+            except TimeoutError as e:
+                pass
+            finally:
+                os.close(pipes['output_w'])
+                return_fd.write(str(ret))
+                return_fd.close()
+                sys.exit()
+        else:
+            res = {}
+            res['status'] = 'timeout'
+            os.close(pipes['output_w'])
+            os.close(pipes['return_w'])
+            output_in = os.fdopen(pipes['output_r'], encoding='cp1252')
+            return_in = os.fdopen(pipes['return_r'])
+            try:
+                with Timeout(t=timeout) as t:
+                    res['output'] = output_in.read()
+                    res['return'] = int(return_in.read())
+                    res['status'] = os.waitpid(pid, 0)[1]
+            except TimeoutError as e:
+                os.kill(pid, signal.SIGTERM)
+                os.waitpid(pid, 0)
+            finally:
+                output_in.close()
+                return_in.close()
+            return res
+
+
         
     def interpret_cmp_results(self, case, res, verbose=False, quiet=False):
         """
         This method manges the display of the results for comparison tests.
         It also stores the success/failure to count it in the final ratio.
         """
+        self.counters['local_tried'] += 1
+        self.counters['global_tried'] += 1
         cols = {}
         m = None
         if res['f2']['status'] != 0:
-            self.counters['err_exit'] += 1
+            self.counters['global_exit_err'] += 1
+            self.counters['local_exit_err'] += 1
         if (res['f1']['status'] == 0 and res['f2']['status'] == 0):
             out_ok = (res['f1']['output'] == res['f2']['output'])
             ret_ok = (res['f1']['return'] == res['f2']['return'])
@@ -228,8 +280,6 @@ class Tester:
             if ret_ok and out_ok:
                 self.counters['local_success'] += 1
                 self.counters['global_success'] += 1
-            self.counters['local_tried'] += 1
-            self.counters['global_tried'] += 1
             if (not (ret_ok and out_ok)) or verbose and not quiet:
                 m = (colorize(self.msgs['test_normal_res'], cols)
                      % (
@@ -282,12 +332,7 @@ if __name__ == '__main__':
     # *********************************************************************** #
 
     parser = argparse.ArgumentParser(
-        description='This is a ft_printf python testing tool.',
-        epilog=(
-            'NB : If none of -c, -s, or -a args is passed to this tool, '
-            'every test sets is run in the following order : comparison, segv,'
-            ' leaks.'
-        ))
+        description='This is a ft_printf python testing tool.')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='this option enables successful results display.')
     parser.add_argument('-q', '--quiet', action='store_true',
@@ -296,13 +341,23 @@ if __name__ == '__main__':
                               'every test set result'))
     parser.add_argument('-u', '--uncolored', dest='colors',
                         action='store_false', help='disable colors.')
-    parser.add_argument('-d', '--debug', dest='debug',
-                        action='store_true', help='enable debug output.')
+    parser.add_argument('-t', '--timeout', dest='t',
+                         type=float, default=0.25,
+                        help=(
+                            'Set timeout duration. 0 will disable short '
+                            'timeout (but a default 5 seconds timeout is still'
+                            ' activated to clean any non-terminated'
+                            ' subprocess if the parent process can\'t kill'
+                            'it in case of timeout).'
+                        ))
     parser.add_argument('filename',
                         help=(
-                            'A valid python3 file containing an iterable '
-                            'called "cases". This iterable will contain '
-                            'several case sets'
+                            'A valid python3 file containing a generator '
+                            'called "cases_generator". This iterable will '
+                            'contain several case sets that will be '
+                            'dictionnaries. These dictionaries will have a'
+                            '\'name\' entry to describe the cases subset, and'
+                            ' a \'cases\' entry '
                         ))
     args = parser.parse_args()
 
@@ -354,4 +409,4 @@ if __name__ == '__main__':
     print(args)
     t = Tester()
     t.run(cases_generator=cases_generator,
-          verbose=args.verbose, quiet=args.quiet, debug=args.debug)
+          verbose=args.verbose, quiet=args.quiet, timeout=args.t)
