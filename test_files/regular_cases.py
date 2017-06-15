88ba275dadfb456d5ba475f005f354970c4a3fd5
Vincent MONTECOT <vmonteco@e2r12p7.42.fr>
2017-06-16 00:09:01 +0200

    some tests added

--- test_files/regular_cases.py
+++ test_files/regular_cases.py
@@ -6,6 +6,16 @@ import test_files.c_cases_regular as c_cases
 import test_files.uc_cases_regular as uc_cases
 import test_files.s_cases_regular as s_cases
 import test_files.us_cases_regular as us_cases
+import test_files.p_cases_regular as p_cases
+import test_files.d_cases_regular as d_cases
+import test_files.ud_cases_regular as ud_cases
+import test_files.i_cases_regular as i_cases
+import test_files.o_cases_regular as o_cases
+import test_files.uo_cases_regular as uo_cases
+import test_files.u_cases_regular as u_cases
+import test_files.uu_cases_regular as uu_cases
+import test_files.x_cases_regular as x_cases
+import test_files.ux_cases_regular as ux_cases
 
 test_sets = (
     percent_cases.test_sets
@@ -18,11 +28,11 @@ test_sets = (
     + ud_cases.test_sets
     + i_cases.test_sets
     + o_cases.test_sets
-    + uo_cases.test_sets#
-    + u_cases.test_sets#
-    + uu_cases.test_sets#
-    + x_cases.test_sets#
-    + ux_cases.test_sets#
+    + uo_cases.test_sets
+    + u_cases.test_sets
+    + uu_cases.test_sets
+    + x_cases.test_sets
+    + ux_cases.test_sets
 )
 
 
