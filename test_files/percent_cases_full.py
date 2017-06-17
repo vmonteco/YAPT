e99dabc8bdbed45c4cd6752abaeb0b07be118a1b
Vincent MONTECOT <vmonteco@e2r12p2.42.fr>
2017-06-17 18:46:06 +0200

    percent tests ok

--- test_files/percent_cases_full.py
+++ test_files/percent_cases_full.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from tools.factories import generator_comb_factory
+from tools.elems import options, min_len, len_mod
 
 test_sets = [
     ('percent tests', b'%', [[]]),
 ]
 
-cases_generator = generator_comb_factory(test_sets)
+def percent_subset_comb_generator_factory(subset):
+    yield from ([b''.join([b'%', opt, ml, lm, subset[1], b'\n']), *v]
+                for opt in options
+                for ml in min_len
+                for lm in len_mod
+                for v in subset[2]
+    )
+
+def generator_percent_comb_factory(test_sets):
+    def f():
+        yield from ({'name': s[0], 'cases': percent_subset_comb_generator_factory(s)} for s in test_sets)
+    return f
+        
+cases_generator = generator_percent_comb_factory(test_sets)
