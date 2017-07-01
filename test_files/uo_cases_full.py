ba6a1660d70567e8909680d1fdba2db7f92ca3f6
Vincent MONTECOT <vmonteco@e3r7p2.42.fr>
2017-07-01 08:22:00 +0200

    modif

--- test_files/uo_cases_full.py
+++ test_files/uo_cases_full.py
@@ -4,7 +4,7 @@ from tools.vals import pos_num_vals as vals
 from tools.factories import generator_comb_factory, subset_comb_generator_factory
 
 test_sets = [
-    ('o tests', b'o', vals, subset_comb_generator_factory),
+    ('O tests', b'O', vals, subset_comb_generator_factory),
 ]
 
 cases_generator = generator_comb_factory(test_sets)
